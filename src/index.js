8da2e08e4b3a5ec11e4af86aff5786b312eea016
guswitch <gustaavohenrique@hotmail.com>
2020-12-21 12:06:45 -0300

    Estrutura criada

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './global.scss';
+
+// Configurando o font awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+library.add(fab, fas);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +16,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
