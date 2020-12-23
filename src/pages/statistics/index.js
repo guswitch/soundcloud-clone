e14de28c96f021f90dec90b3a72ca6a67aa62f78
guswitch <gustaavohenrique@hotmail.com>
2020-12-22 21:00:25 -0300

    Updates

--- src/pages/statistics/index.js
+++
@@ -1,9 +0,0 @@
-import './style.scss';
-
-export default function Statistics() {
-    return (
-      <div className="Statistics">
-       <h1> Statistics </h1>
-      </div>
-    );
-  }
--- src/pages/stream/index.js
+++
@@ -1,9 +0,0 @@
-import './style.scss';
-
-export default function Stream() {
-    return (
-      <div className="Stream">
-       <h1> Stream </h1>
-      </div>
-    );
-  }
